c01c7ee599fa47cbbc2fd89cfec7e460aa199c80
Prakash <fku2vr@virginia.edu>
2023-12-07 22:00:16 -0500

    Change mapping function and make new mapping

--- lua/tepl.lua
+++ lua/tepl.lua
@@ -9,8 +9,7 @@ local M = {}
 
 local config = {
     shell ={
-        default =  "python",
-        cpp = "reroot -l",
+        default = "python", cpp = "reroot -l",
         julia = "julia",
         python = "python",
     }
@@ -22,7 +21,7 @@ local function on_close()
 end
 
 local function open_window(shell)
-    if shell == nil then shell = config.shell.default end
+    shell = shell or config.shell.default
     vim.cmd('split')
     local win = vim.api.nvim_get_current_win()
     buf = vim.api.nvim_create_buf(true, true)
@@ -32,42 +31,20 @@ local function open_window(shell)
 end
 
 local function run_command(lines,jobid)
-    if termid == nil then print("No Active") else
+    if termid == nil then print("No Active REPL") else
         local str = ""
-        for _,ln in ipairs(lines) do str = str..ln.."\n" end
+        for _,ln in ipairs(lines) do if ln:gsub("%s+","") ~= "" then str = str..ln.."\n" end end
         vim.api.nvim_chan_send(jobid,str)
     end
 end
 
-local function execute_cur_line()
-    local str = vim.api.nvim_get_current_line()
-    run_command(str.."\n",termid)
-end
-
--- https://stackoverflow.com/a/72197874
-local run_motion = function ()
-  local old_func = vim.go.operatorfunc
-  _G.op_run_motion = function()
-
+function Execute_lines()
     local start = vim.fn.getpos("'[")[2]
     local finish  = vim.fn.getpos("']")[2]
     local lines = vim.fn.getline(start,finish)
 
     run_command(lines,termid)
-
-    vim.go.operatorfunc = old_func
-    _G.op_run_motion = nil
-  end
-  vim.go.operatorfunc = 'v:lua.op_run_motion'
 end
-
-M.operator =  function(motion)
-    return function()
-        run_motion()
-        vim.api.nvim_feedkeys('g@'..(motion or ''), 'n', false)
-    end
-end
-
 
 M.whid = function(arg)
   if termid ~= nil then print("There is an existing REPL") else termid = open_window(arg) end
@@ -75,18 +52,16 @@ end
 
 M.setup = function(configp)
     if configp ~= nil then config = utl.merge(config,configp) end
-    vim.keymap.set("n", "gz", M.operator(),{noremap=true}) --, {noremap = true})
-    vim.keymap.set("n", "gzz", M.operator("_j"),{noremap=true}) --, {noremap = true})
-    vim.keymap.set("n", "<leader><cr>", M.operator("ap}"),{noremap=true}) --, {noremap = true})
+    vim.go.operatorfunc = "v:lua.Execute_lines"
+    vim.keymap.set("n", "gz", "g@")
+    vim.keymap.set("n", "gzz", "g@_")
+    vim.keymap.set("n", "<leader><cr>", "g@ap}",{remap=true})
 
-    local command = vim.api.nvim_create_user_command
-    command("Tepl",function(opts)
-        local shell = opts.args
-        if shell == nil or shell == '' then
-            shell = config.shell[vim.bo.filetype] or config.shell.default
-        end
-        require("tepl").whid(shell)
-    end, {nargs='?'})
+    vim.api.nvim_create_user_command("Tepl",function(opts)
+        local shell = opts.args or ''
+        if shell == '' then shell =  config.shell[vim.bo.filetype] or config.shell.default end
+        M.whid(shell)
+    end, {nargs='*'})
 end
 
 return M
--- lua/tepl/utils.lua
+++ lua/tepl/utils.lua
@@ -5,12 +5,12 @@ local M = {}
 M.tprint = function(tbl, indent)
     if not indent then indent = 0 end
     for k, v in pairs(tbl) do
-        formatting = string.rep("  ", indent) .. k .. ": "
+        local formatting = string.rep("  ", indent) .. k .. ": "
         if type(v) == "table" then
             print(formatting)
             M.tprint(v, indent+1)
         elseif type(v) == 'boolean' then
-            print(formatting .. tostring(v))      
+            print(formatting .. tostring(v))
         else
             print(formatting .. v)
         end
